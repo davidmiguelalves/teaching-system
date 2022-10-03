f1d78fe3029e5c4d5776d5d219fa43f2f35e1fb3
David Alves <david.alves@primaverabss.com>
2022-10-04 00:16:22 +0100

    diagram  + interface

---
+++ ObjectDetectionAPP/ObjectDetection/API/IObjectDetectionAPI.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using ObjectDetection.Models;
+using ObjectDetection.Others;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDetection.API
+{
+    public interface IObjectDetectionAPI
+    {
+        List<ObjectDetected> DetectObject(string activity, byte[] byteArray);
+        List<string> GetActivities();
+        List<string> GetObjects(string activity);
+        bool Probe();
+    }
+}
--- ObjectDetectionAPP/ObjectDetection/API/ObjectDetectionAPI.cs
+++ ObjectDetectionAPP/ObjectDetection/API/ObjectDetectionAPI.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ObjectDetection.API
 {
-    public class ObjectDetectionAPI
+    public class ObjectDetectionAPI : IObjectDetectionAPI
     {
         public string URL { get; set; } = "http://127.0.0.1:5000";
 
@@ -102,5 +102,6 @@ namespace ObjectDetection.API
             }
             return ret;
         }
+
     }
 }
