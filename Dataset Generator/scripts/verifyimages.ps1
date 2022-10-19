4283cba91088d9f87d23f9f96a27ddaa122027ad
David Alves <david.alves@primaverabss.com>
2022-10-19 21:16:47 +0100

    change

--- Dataset Generator/scripts/verifyimages.ps1
+++ Dataset Generator/scripts/verifyimages.ps1
@@ -1,22 +1,40 @@
-﻿param(
-     [Parameter()]
-     [string]$folder
-)
+﻿
+$foldertoverify = "D:\Tese\colors\full-recise"
 
-<#
-$folder = "D:\Tese\shapes\teste"
-#>
+$verifyfolder = "D:\Tese\tese\verify"
+$folder0 = $verifyfolder+"\0"
+$folder1 = $verifyfolder+"\1"
+$folder2 = $verifyfolder+"\2"
 
-Get-ChildItem $folder -Include *.jpg | 
+
+if (-not (Test-Path -Path $verifyfolder)) {
+    $ret = New-Item $verifyfolder -ItemType Directory
+}
+if (-not (Test-Path -Path $folder0)) {
+    $ret = New-Item $folder0 -ItemType Directory
+}
+if (-not (Test-Path -Path $folder1)) {
+    $ret = New-Item $folder1 -ItemType Directory
+}
+if (-not (Test-Path -Path $folder2)) {
+    $ret = New-Item $folder2 -ItemType Directory
+}
+
+
+Get-ChildItem $foldertoverify -Include *.jpg -Recurse | 
 Foreach-Object {
     $file = $_.FullName
-
-#verify if txt file exists!!!
     $content = Get-Content $file.Replace(".jpg",".txt")
-    $destfolder = $folder + "\" + $content.split(" ")[0]
-
-    if (-not (Test-Path -Path $destfolder)) {
-        $ret = New-Item $destfolder -ItemType Directory
+    if($content.StartsWith("0")){
+        Copy-item -Path $file -Destination $folder0
+        Copy-item -Path $file.Replace(".jpg",".txt") -Destination $folder0
+    }elseif($content.StartsWith("1")){
+        Copy-item -Path $file -Destination $folder1
+        Copy-item -Path $file.Replace(".jpg",".txt") -Destination $folder1
+    }elseif($content.StartsWith("2")){
+        Copy-item -Path $file -Destination $folder2
+        Copy-item -Path $file.Replace(".jpg",".txt") -Destination $folder2
+    }else{
+        Write-Error "ERROR"
     }
-    Copy-item -Path $file -Destination $destfolder -Force
 }
